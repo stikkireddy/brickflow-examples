a444c40ce49ddfc3bf30c4e1d306befa8aaab95c
stikkireddy <sri.tikkireddy@databricks.com>
2022-09-30 09:22:30 -0400

    changed to 30 tables

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         dbutils.data.summarize(spark.table("diamonds"))
 
 
-    read_tasks = [f"read_table_{i}"for i in range(5)]
+    read_tasks = [f"read_table_{i}"for i in range(30)]
     for t in read_tasks:
         @wf.task(name=t, depends_on=[analyze_table])
         def read_table(*, test=1234):
