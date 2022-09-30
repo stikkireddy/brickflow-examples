b3251be3631e2ce6e3b0422daf8553b06a9c196b
stikkireddy <sri.tikkireddy@databricks.com>
2022-09-30 13:24:23 -0400

    updated with dq checks

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ if __name__ == "__main__":
         print("reading csv file")
         return file
 
+
+    # @wf.bind_airflow_task(dag, "task_id")
     @wf.task()
     def dummy_task():
         print("dummy_task")
@@ -28,10 +30,12 @@ if __name__ == "__main__":
         return "debug"
 
     @wf.task()
+    # @wf.bind_airflow_task(dag, "task_1243")
     def read_csv_task():
-        print("dummy_task")
-        read_csv("file://some other file")
-        return "debug"
+        pass
+        # print("dummy_task")
+        # read_csv("file://some other file")
+        # return "debug"
 
 
     @wf.task(depends_on=[read_csv_task])
@@ -39,14 +43,22 @@ if __name__ == "__main__":
         dbutils.data.summarize(spark.table("diamonds"))
 
 
-    read_tasks = [f"read_table_{i}"for i in range(10)]
+    read_tasks = [f"read_table_{i}"for i in range(3)]
+    dq_checks = []
     for t in read_tasks:
         @wf.task(name=t, depends_on=[analyze_table])
         def read_table(*, test=1234):
             if t == "read_table_1":
                 spark.table("diamonds").limit(10).display()
 
-    @wf.task(depends_on=read_tasks)
+
+        dq_checks.append(f"dq_{t}")
+        @wf.task(name=f"dq_{t}", depends_on=[t])
+        def read_table2(*, test=1234):
+            if t == "read_table_1":
+                spark.table("diamonds").limit(10).display()
+
+    @wf.task(depends_on=dq_checks)
     def write_table(*, test=1234):
         spark.table("diamonds").write.mode("overwrite").saveAsTable("sri_demo.diamonds_brickflow")
 
